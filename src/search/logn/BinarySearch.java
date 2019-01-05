a4899d35b3807540891126021a6c7260e0d936fc
leezer <li.zenan@tianyuantechnology.com>
2019-01-05 20:57:35 +0800

    ADD: 1. 二分查找问题详解

--- src/search/logn/BinarySearch.java
+++ src/search/logn/BinarySearch.java
@@ -21,44 +21,45 @@ public class BinarySearch {
 
 
     public static void main(String[] args) {
-        int[] ints = {13, 16, 18, 26, 29, 29, 37, 38, 47, 56, 63, 64,  77, 80, 88, 90, 96, 99, 100,107, 107, 109, 112, 115, 119, 122, 129, 131, 134, 141, 148, 152, 159, 161, 167, 172, 178, 187, 191, 194, 200, 206, 214, 216, 225, 228, 232, 237, 246, 246, 251, 252, 254, 256, 257, 263, 272, 278, 283, 287, 291, 297, 303, 312, 314, 320, 324, 326, 328, 328, 331, 331, 332, 335, 344, 353, 360, 369, 373, 374, 383, 383, 392, 395, 401, 405, 411, 411, 413, 416, 423, 431, 435, 435, 443, 444, 445, 451, 459, 465, 467};
+        int[] ints = {10, 27, 28, 29, 29, 30,38};
         System.out.println(Arrays.toString(ints));
-        int i = simpleBinarySearch(100, ints);
-        int i1 = recursiveBinarySearch(0,ints.length  -1 ,100, ints);
+        int i = simpleBinarySearch(29, ints);
         System.out.println(i);
+        int i1 = binaryLastValue(29, ints);
         System.out.println(i1);
-       solve();
+        int i2 = binaryEqAndLessValue(29,ints);
+        System.out.println(i2);
     }
-
 
 
     /**
      * 二分查找数组中某个值是否在内
+     *
      * @param array 查找数组
-     * @param i  查找元素
+     * @param i     查找元素
      * @return 返回数组下标
      **/
-    public static int simpleBinarySearch(int i,int[] array){
+    public static int simpleBinarySearch(int i, int[] array) {
 
-        if (array.length == 1){
-            if (array[0] == i){
+        if (array.length == 1) {
+            if (array[0] == i) {
                 return 0;
             }
         }
-        
+
         int low = 0;
-        int high = array.length-1;
+        int high = array.length - 1;
 
         //判断条件必须加上等于过一遍 反之发生死循环
-        while (low<=high){
+        while (low <= high) {
             //确保数据太大不会溢出
-            int mid = (low + (high - low) /2);
-            if (array[mid] == i){
+            int mid = (low + (high - low) / 2);
+            if (array[mid] == i) {
                 return mid;
             }//如果中间值大于i说明在[low,mid)区间里面
-            else if (array[mid] > i){
-                high = mid -1 ;
-            }else {
+            else if (array[mid] > i) {
+                high = mid - 1;
+            } else {
                 low = mid + 1;
             }
         }
@@ -67,56 +68,190 @@ public class BinarySearch {
     }
 
 
-
     /**
      * 二分查找数组某个值是否存在
      * 递归版
+     *
      * @param array 数组
-     * @param low 最小值
-     * @param high 最大值
+     * @param low   最小值
+     * @param high  最大值
      * @param value 是否存在的值
      * @return 返回结果
      **/
-    public static int recursiveBinarySearch(int low,int high,int value,int[] array){
+    public static int recursiveBinarySearch(int low, int high, int value, int[] array) {
         //确定终止条件
 
-        if (low > high){
-            return  -1;
+        if (low > high) {
+            return -1;
         } else {
             //算出中间值
             int mid = low + ((high - low) >> 1);
 
-            if (array[mid] == value){
+            if (array[mid] == value) {
                 return mid;
-            }else if (value < array[mid]){
+            } else if (value < array[mid]) {
                 high = mid - 1;
-                return recursiveBinarySearch(low,high,value,array);
-            }else {
+                return recursiveBinarySearch(low, high, value, array);
+            } else {
                 low = mid + 1;
-                return recursiveBinarySearch(low,high,value,array);
+                return recursiveBinarySearch(low, high, value, array);
             }
         }
     }
 
+
+    /**
+     * 查找第一个值等于给定元素的值
+     *
+     * @param value 查找值
+     * @param array 查找数组
+     **/
+    public static int binaryFirstValue(int value, int[] array) {
+
+        int low = 0;
+        int high = array.length - 1;
+        int result = -1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] == value) {
+                high = mid - 1;
+                result = mid;
+            } else if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 查找最后一个值等于给定元素的值
+     *
+     * @param value 查找值
+     * @param array 查找数组
+     **/
+    public static int binaryLastValue(int value, int[] array) {
+
+        int low = 0;
+        int high = array.length - 1;
+        int result = -1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] == value) {
+                low = mid + 1;
+                result = mid;
+            } else if (array[mid] > value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 查找第一个值大于等于给定元素的值
+     *
+     * @param value 查找值
+     * @param array 查找数组
+     **/
+    public static int binaryEqAndMoreValue(int value, int[] array) {
+
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] <= value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+        if (array[high] == value) {
+            return high + 1;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * 查找第一个值大于等于给定元素的值
+     *
+     * @param value 查找值
+     * @param array 查找数组
+     **/
+    public static int binaryEqAndLessValue(int value, int[] array) {
+
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] >= value) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        if (array[low] == value) {
+            return low - 1;
+        } else {
+            return -1;
+        }
+    }
+
+
+    /**
+     * 查找第一个值等于给定元素的值(递归版)还有问题
+     *
+     * @param value 查找值
+     * @param array 查找数组
+     **/
+    public static int binaryFirstValueInternally(int[] array, int value, int low, int high) {
+        int result = -1;
+        if (low > high) return result;
+
+        int mid = low + ((high - low) >> 1);
+        if (array[mid] == value) {
+            high = mid - 1;
+            result = binaryFirstValueInternally(array, value, low, high);
+            return result;
+        } else if (array[mid] > value) {
+            high = mid - 1;
+            return binaryFirstValueInternally(array, value, low, high);
+        } else {
+            low = mid + 1;
+            return binaryFirstValueInternally(array, value, low, high);
+        }
+
+
+    }
 
 
     /**
      * extend 扩展
      * 假设我们有 1000 万个整数数据，每个数据占 8 个字节，
      * 如何设计数据结构和算法，快速判断某个 整数是否出现在这 1000 万数据中？内存限制在100MB左右
+     * 使用散列表和二叉树需要额外申请空间 所以在这里并不适用
      **/
-    public static void solve(){
+    public static void solve() {
         //进行查找
         Random random = new Random();
         int[] ints = BaseUtil.randomIntArray(10000000, 10000000);
         long l = System.currentTimeMillis();
         //先对数据进行排序
-        QuickSort.quickSort(ints,ints.length -1 );
+        QuickSort.quickSort(ints, ints.length - 1);
         long l1 = System.currentTimeMillis();
         System.out.println(String.format("排序耗费:%s毫秒", l1 - l));
         //进行查找
 //        int i = simpleBinarySearch(ints[random.nextInt(3000)], ints);
-        int i = recursiveBinarySearch(0,ints.length -1 ,ints[random.nextInt(3000)], ints);
+        int i = recursiveBinarySearch(0, ints.length - 1, ints[random.nextInt(3000)], ints);
         System.out.println(String.format("查找耗费:%s毫秒", System.currentTimeMillis() - l1));
         System.out.println(i);
 
